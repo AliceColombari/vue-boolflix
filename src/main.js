43908552c62ebfd05db9c31788f09b6c2209c9ef
AliceColombari <colombari.23@gmail.com>
2022-02-28 09:41:44 +0100

    import font awesome in main.js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,7 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
